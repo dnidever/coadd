32cd840295a0ac164bd303d73e93c896693b4182
David Nidever <dnidever@lsst.org>
2022-09-13 11:10:47 -0600

    bumped version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='coadd',
-      version='1.0.0',
+      version='1.0.1',
       description='Coadd images',
       author='David Nidever',
       author_email='dnidever@montana.edu',
